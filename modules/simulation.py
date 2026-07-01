55ade9f31472bb5f2c6287a772b9524dd318d00c
simon-krey <simonkrey2009@icloud.com>
2026-07-01 16:21:36 +0200

    added start and end nodes

--- modules/simulation.py
+++ modules/simulation.py
@@ -2,7 +2,15 @@ from modules.gates import gates
 
 class Simulation:
     def __init__(self):
-        self.logic = [
+        self.start_nodes = [
+
+        ]
+
+        self.logic_nodes = [
             # {"type" : "and", "layer" : 0, "inputs": [1, 2], "outputs": [3], "out_active": False}
+        ]
+
+        self.end_nodes = [
+            
         ]
 
